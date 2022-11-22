13f158ce7691a45e6797c5d0331b8177aaa6946d
Jay Gazula <jaygazula27@gmail.com>
2022-11-22 00:36:11 -0500

    Converted to multi-module project

---
+++ e2e/target/generated-sources/typesafe-resources/com/jgazula/DatabaseProperties.java
@@ -0,0 +1,13 @@
+package com.jgazula;
+
+import java.lang.String;
+
+public class DatabaseProperties {
+    public static final String SPRING_DATASOURCE_DRIVER_CLASS_NAME = "spring.datasource.driver-class-name";
+
+    public static final String SPRING_DATASOURCE_USERNAME = "spring.datasource.username";
+
+    public static final String SPRING_DATASOURCE_URL = "spring.datasource.url";
+
+    public static final String SPRING_DATASOURCE_PASSWORD = "spring.datasource.password";
+}
---
+++ e2e/target/generated-sources/typesafe-resources/com/jgazula/MainProperties.java
@@ -0,0 +1,11 @@
+package com.jgazula;
+
+import java.lang.String;
+
+public class MainProperties {
+    public static final String APP_AUTHOR_FIRST_NAME = "app.author.first.name";
+
+    public static final String APP_AUTHOR_LAST_NAME = "app.author.last.name";
+
+    public static final String APP_VERSION = "app.version";
+}
